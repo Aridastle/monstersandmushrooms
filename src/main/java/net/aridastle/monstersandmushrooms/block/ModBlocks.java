8c252ac29a23811caf19bf3344e12e0bd3086105
steel380 <steel380@umn.edu>
2022-10-21 20:18:10 -0600

    Blocks fully implemented with recipes and loot tables.

--- src/main/java/net/aridastle/monstersandmushrooms/block/ModBlocks.java
+++ src/main/java/net/aridastle/monstersandmushrooms/block/ModBlocks.java
@@ -23,15 +23,15 @@ public class ModBlocks {
             DeferredRegister.create(ForgeRegistries.BLOCKS, monstersandmushrooms.MOD_ID);
 
     public static final RegistryObject<Block> ROCKY_MOSS = registerBlock("rocky_moss",
-            () -> new Block(BlockBehaviour.Properties.of(Material.MOSS).sound(SoundType.MOSS)), ModCreativeModeTab.MONSTERSANDMUSHROOMS_TAB);
+            () -> new Block(BlockBehaviour.Properties.of(Material.MOSS).sound(SoundType.MOSS).strength(0.25f)), ModCreativeModeTab.MONSTERSANDMUSHROOMS_TAB);
     public static final RegistryObject<Block> CALCITE_BRICKS = registerBlock("calcite_bricks",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).sound(SoundType.CALCITE).requiresCorrectToolForDrops()), ModCreativeModeTab.MONSTERSANDMUSHROOMS_TAB);
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).sound(SoundType.CALCITE).strength(1f).requiresCorrectToolForDrops()), ModCreativeModeTab.MONSTERSANDMUSHROOMS_TAB);
     public static final RegistryObject<Block> COBBLED_COAL = registerBlock("cobbled_coal",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).sound(SoundType.STONE).requiresCorrectToolForDrops()), ModCreativeModeTab.MONSTERSANDMUSHROOMS_TAB);
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).sound(SoundType.STONE).strength(4f).requiresCorrectToolForDrops()), ModCreativeModeTab.MONSTERSANDMUSHROOMS_TAB);
     public static final RegistryObject<Block> ICY_DEEPSLATE_TILES = registerBlock("icy_deepslate_tiles",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).sound(SoundType.DEEPSLATE_TILES).requiresCorrectToolForDrops()), ModCreativeModeTab.MONSTERSANDMUSHROOMS_TAB);
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).sound(SoundType.DEEPSLATE_TILES).strength(4f).requiresCorrectToolForDrops()), ModCreativeModeTab.MONSTERSANDMUSHROOMS_TAB);
     public static final RegistryObject<Block> POLISHED_AMETHYST = registerBlock("polished_amethyst",
-            () -> new Block(BlockBehaviour.Properties.of(Material.AMETHYST).sound(SoundType.AMETHYST).requiresCorrectToolForDrops()), ModCreativeModeTab.MONSTERSANDMUSHROOMS_TAB);
+            () -> new Block(BlockBehaviour.Properties.of(Material.AMETHYST).sound(SoundType.AMETHYST).strength(2f).requiresCorrectToolForDrops()), ModCreativeModeTab.MONSTERSANDMUSHROOMS_TAB);
 
     private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
